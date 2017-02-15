0131b909699b3272c9192e5f833c8186be6aa091
Matt Hinz <matt@toolhouse.com>
2017-02-15 14:40:45 -0800

    Tweaks to packaging script

--- package.cmd
+++ package.cmd
@@ -6,6 +6,7 @@ call build.cmd || exit /B 101
 rmdir /S /Q Dist
 mkdir Dist
 
+echo Packaging...
 pushd Toolhouse.Monitoring
 nuget pack ^
     -OutputDirectory ..\Dist ^
@@ -13,10 +14,18 @@ nuget pack ^
     Toolhouse.Monitoring.csproj ^
     || (popd && exit /B 201)
 
+popd
+echo Done packaging
+
 pushd Dist
+set pkg=notfound
 for %%f in (*.nupkg) do (
     set pkg=%%f
 ) || (popd && exit /B 301)
+
+if "%pkg%"=="notfound" (
+    echo Package file not found && popd && exit /B 401
+)
 
 echo Package built. To push to nuget, run the following:
 echo nuget push Dist\%pkg% -Source https://www.nuget.org/api/v2/package
