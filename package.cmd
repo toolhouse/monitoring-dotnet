1e8d7b55d4e87fd90bfb0d1c2e55e764217b7bbd
Matt Hinz <matt@toolhouse.com>
2017-02-15 13:46:48 -0800

    Update package command

--- package.cmd
+++ package.cmd
@@ -1,10 +1,19 @@
 REM Builds the project's nuget package.
+@echo off
 
 call build.cmd || exit /B 101
 
+rmdir /S /Q Dist
 mkdir Dist
 
 pushd Toolhouse.Monitoring
 nuget pack -OutputDirectory ..\Dist Toolhouse.Monitoring.csproj || (popd && exit /B 201)
 
+pushd Dist
+for %%f in (*.nupkg) do (
+    set pkg=%%f
+) || (popd && exit /B 301)
+
+echo Package built. To push to nuget, run the following:
+echo nuget push Dist\%pkg% -Source https://www.nuget.org/api/v2/package
 popd
